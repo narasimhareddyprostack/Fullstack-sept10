fa014fbc5d8588b7e237ab2b5ca7f18cf51644bf
narasimhareddyprostack <narasimhareddyk18@gmail.com>
2020-11-05 15:02:31 +0530

    ReactJS - second

---
+++ mcart/src/Pooja/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Order from "./Order";
+function App() {
+  return (
+    <>
+      <h1> Welcome to PSA : Pooja Folder</h1>
+    </>
+  );
+}
+export default App;
---
+++ mcart/src/Pooja/Order.js
@@ -0,0 +1,5 @@
+import React from "react";
+function Order(){
+    return <h4> Order Successfully Delivered</h4>
+}
+export default Order
